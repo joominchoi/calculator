a65692374624f2a255573e7f8d4b8222be9fb1aa
JooMin Choi <joominchoi23@gmail.com>
2023-08-18 11:45:45 +0100

    Hardcode eventlisteners for all numbers

--- main.js
+++ main.js
@@ -10,11 +10,66 @@ let displayValue = displayCurrent.innerHTML
 
 console.log(displayValue)
 
+zero.addEventListener("click", function() {
+  displayValue = zero.innerHTML
+  updateDisplay(zero.innerHTML)
+  console.log(displayValue)
+});
+
 one.addEventListener("click", function() {
   displayValue = one.innerHTML
   updateDisplay(one.innerHTML)
   console.log(displayValue)
 });
+
+two.addEventListener("click", function() {
+  displayValue = two.innerHTML
+  updateDisplay(two.innerHTML)
+  console.log(displayValue)
+});
+
+three.addEventListener("click", function() {
+  displayValue = three.innerHTML
+  updateDisplay(three.innerHTML)
+  console.log(displayValue)
+});
+
+four.addEventListener("click", function() {
+  displayValue = four.innerHTML
+  updateDisplay(four.innerHTML)
+  console.log(displayValue)
+});
+
+five.addEventListener("click", function() {
+  displayValue = five.innerHTML
+  updateDisplay(five.innerHTML)
+  console.log(displayValue)
+});
+
+six.addEventListener("click", function() {
+  displayValue = six.innerHTML
+  updateDisplay(six.innerHTML)
+  console.log(displayValue)
+});
+
+seven.addEventListener("click", function() {
+  displayValue = seven.innerHTML
+  updateDisplay(seven.innerHTML)
+  console.log(displayValue)
+});
+
+eight.addEventListener("click", function() {
+  displayValue = eight.innerHTML
+  updateDisplay(eight.innerHTML)
+  console.log(displayValue)
+});
+
+nine.addEventListener("click", function() {
+  displayValue = nine.innerHTML
+  updateDisplay(nine.innerHTML)
+  console.log(displayValue)
+});
+
 
 function updateDisplay(value) {
   displayCurrent.innerHTML = value
